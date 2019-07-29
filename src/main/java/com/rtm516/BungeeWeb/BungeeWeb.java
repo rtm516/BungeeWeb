1753d15975a39e28db9712050fb30e98e680fdc2
Ryan Milner <ryantmilner@hotmail.co.uk>
2019-07-29 19:32:29 +0100

    Added custom build and updated HTTP Server

--- src/main/java/com/rtm516/BungeeWeb/BungeeWeb.java
+++ src/main/java/com/rtm516/BungeeWeb/BungeeWeb.java
@@ -1,18 +1,53 @@
 package com.rtm516.BungeeWeb;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
 
 public class BungeeWeb extends Plugin {
+	private Configuration configuration;
+	
     @Override
-    public void onEnable() {
-        // You should not put an enable message in your plugin.
-        // BungeeCord already does so
-        getLogger().info("Yay! It loads!");
-        HTTPServer.start();
+    public void onEnable() {        
+        setupConfig();
+        
+        HTTPServer.start(configuration.getInt("port"));
     }
     
     @Override
     public void onDisable() {
     	HTTPServer.stop();
+    }
+    
+    private void setupConfig() {
+    	if (!getDataFolder().exists())
+            getDataFolder().mkdir();
+
+        File file = new File(getDataFolder(), "config.yml");
+
+   
+        if (!file.exists()) {
+            try (InputStream in = getResourceAsStream("config.yml")) {
+                Files.copy(in, file.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        try {
+			configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+			
+			getProxy().getPluginManager().unregisterListeners(this);
+	        getProxy().getPluginManager().unregisterCommands(this);
+			this.onDisable();
+		}
     }
 }
--- src/main/java/com/rtm516/BungeeWeb/HTTPServer.java
+++ src/main/java/com/rtm516/BungeeWeb/HTTPServer.java
@@ -6,17 +6,20 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class HTTPServer {
-	static final int port = 8080;
 	static HttpServer server;
 
-	public static void start() {
+	public static void start(int port) {
+		if (port <= 0) {
+			port = 8080;
+		}
+		
 		try {
 			server = HttpServer.create(new InetSocketAddress(port), 0);
-
+			
 			server.createContext("/", httpExchange -> {
-				byte response[] = "Hello, World!".getBytes("UTF-8");
+				byte response[] = renderHome().getBytes("UTF-8");
 
-				httpExchange.getResponseHeaders().add("Content-Type", "text/plain; charset=UTF-8");
+				httpExchange.getResponseHeaders().add("Content-Type", "text/html; charset=UTF-8");
 				httpExchange.sendResponseHeaders(200, response.length);
 
 				OutputStream out = httpExchange.getResponseBody();
@@ -24,8 +27,8 @@ public class HTTPServer {
 				out.close();
 			});
 			
-			server.createContext("/", httpExchange -> {
-				byte response[] = "Hello, World!".getBytes("UTF-8");
+			server.createContext("/content/", httpExchange -> {
+				byte response[] = "test".getBytes("UTF-8");
 
 				httpExchange.getResponseHeaders().add("Content-Type", "text/plain; charset=UTF-8");
 				httpExchange.sendResponseHeaders(200, response.length);
@@ -42,7 +45,30 @@ public class HTTPServer {
 	}
 	
 	public static void stop() {
-		server.stop(0);
+		if (server != null) {
+			server.stop(0);
+		}
 	}
-
+	
+	private static String renderHome() {
+		String content = "";
+		
+		content += "<!DOCTYPE html>\n" + 
+				"<html>\n" + 
+				"<head>\n" + 
+				"	<meta charset='utf-8'>\n" + 
+				"	<meta http-equiv='X-UA-Compatible' content='IE=edge'>\n" + 
+				"	<title>Page Title</title>\n" + 
+				"	<meta name='viewport' content='width=device-width, initial-scale=1'>\n" + 
+				"	<link rel='stylesheet' type='text/css' media='screen' href='main.css'>\n" + 
+				"</head>\n" + 
+				"<body>\n";
+		
+		content += "Test\n";
+		
+		content += "</body>\n" + 
+				"</html>";
+		
+		return content;
+	}
 }
